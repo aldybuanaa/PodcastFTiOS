2925ce2b39c363659d301d7b13b3293d8fdcf55d
aldybuana <aldybuana11@gmail.com>
2022-11-13 21:09:03 +0700

    tabBar

--- PodcastFTiOS/PodcastFTiOS/Modules/Main/MainViewController.swift
+++ PodcastFTiOS/PodcastFTiOS/Modules/Main/MainViewController.swift
@@ -7,13 +7,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setup()
     }
-
+    
+    func setup() {
+        tabBar.tintColor = UIColor(rgb: 0xCBFB5E)
+        tabBar.unselectedItemTintColor = UIColor(rgb: 0x71737B)
+    }
 
 }
 
